18065d2e8f0509d0fd9b365bea8c9bff709268b6
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-06-24 23:13:17 +0200

    Add XMLComment.ts

---
+++ src/XMLComment.ts
@@ -0,0 +1,47 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  XML TypeScript Library
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License; you may not use this file except in
+  compliance with the License. You may obtain a copy of the MIT License at
+  https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Declarations
+ *
+ * */
+
+
+
+/**
+ * Represents an XML comment node.
+ */
+export interface XMLComment {
+
+    /**
+     * Text of the comment.
+     */
+    comment: string;
+
+    /**
+     * Use this property to determine, if the object is a tag node.
+     */
+    tag?: undefined;
+
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default XMLComment;
