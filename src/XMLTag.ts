076f0e9f5ffb0bb8af999f30d07f534a51647d74
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-06-24 23:11:55 +0200

    Add XMLTag.ts

---
+++ src/XMLTag.ts
@@ -0,0 +1,76 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  XML TypeScript Library
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License; you may not use this file except in
+  compliance with the License. You may obtain a copy of the MIT License at
+  https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+
+import XMLNode from './XMLNode.js';
+
+
+/* *
+ *
+ *  Declarations
+ *
+ * */
+
+
+/**
+ * Represent an XML tag.
+ */
+export interface XMLTag {
+
+    /**
+     * Attributes of the XML tag. Attribute names might include a namespace.
+     */
+    attributes?: Record<string, string>;
+
+    /**
+     * Use this property to determine, if the object is a comment node.
+     */
+    comment?: undefined;
+
+    /**
+     * Indicates a self-closing XML tag.
+     */
+    empty?: boolean;
+
+    /**
+     * Encapsuled inner XML nodes, if the XML tag is part of an XMLTree.
+     */
+    innerXML?: Array<XMLNode>;
+
+    /**
+     * Encapsuled inner text, if the XML tag is part of an XMLTree.
+     */
+    innerText?: string;
+
+    /**
+     * Name of the XML tag. The name might include a namespace.
+     */
+    tag: string;
+
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default XMLTag;
